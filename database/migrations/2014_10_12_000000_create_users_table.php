1b70fd3420bf9d6c4724d960ccf48fcf351d24c7
AxelAlemanD <axelad38@gmail.com>
2022-04-28 10:40:30 -0500

    fix: intermediate table migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,8 +20,6 @@ return new class extends Migration
             $table->string('phone_number', 15)->nullable();
             $table->string('address')->nullable();
             $table->string('username');
-            // $table->string('email')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- database/migrations/2022_04_27_205545_create_category_product_table.php
+++ database/migrations/2022_04_27_205545_create_category_product_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('product_category', function (Blueprint $table) {
+        Schema::create('category_product', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->foreignId('product_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product_category');
+        Schema::dropIfExists('category_product');
     }
 };
