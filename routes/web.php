157ca850a09078357f3f36f8a97723353be3ad83
AxelAlemanD <axelad38@gmail.com>
2022-04-28 19:21:53 -0500

    add: products routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
+
+
+Route::resource('productos', ProductsController::class);
