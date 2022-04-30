775a7c5e5196d15c48a5fd5fddad2610b88966b2
AxelAlemanD <axelad38@gmail.com>
2022-04-30 09:58:17 -0500

    add: customer index

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CustomersController extends Controller
@@ -13,7 +14,8 @@ class CustomersController extends Controller
      */
     public function index()
     {
-        //
+        $customers = User::all();
+        return view('Customers.index', get_defined_vars());
     }
 
     /**
