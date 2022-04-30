09e028b8d993a1109a85d1abbfd1df0f9620bdf4
AxelAlemanD <axelad38@gmail.com>
2022-04-30 11:43:41 -0500

    add: links to sales module in sidemenu

--- resources/views/components/sidemenu.blade.php
+++ resources/views/components/sidemenu.blade.php
@@ -65,7 +65,7 @@
 
 			{{-- Ventas --}}
 			<li class="slide">
-				<a class="side-menu__item" data-toggle="slide" href="#" id="sales">
+				<a class="side-menu__item {{ Request::is('ventas') ? 'active' : '' }}" data-toggle="slide" href="{{route('ventas.index')}}" id="sales">
 					<i class="sidemenu_icon">
 						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
 						</svg>
