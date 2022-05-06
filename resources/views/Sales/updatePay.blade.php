c793e5d6e5524d173045204e7974be8170025a4f
AxelAlemanD <axelad38@gmail.com>
2022-05-05 19:54:48 -0500

    style: Change modal name

--- resources/views/Sales/updatePay.blade.php
+++ resources/views/Sales/updatePay.blade.php
@@ -1,5 +1,5 @@
 
-<div class="modal" tabindex="-1" role="dialog" id="createPublication">
+<div class="modal" tabindex="-1" role="dialog" id="updatePay">
     <div class="modal-dialog modal-dialog-centered modal-xl " role="document">
         <div class="modal-content">
             <div class="modal-header">
