73105160834dfd9e0f087e19bc47ee8f5e5b9085
AxelAlemanD <axelad38@gmail.com>
2022-05-07 12:33:02 -0500

    fix: button to close modal

--- resources/views/Sales/updatePay.blade.php
+++ resources/views/Sales/updatePay.blade.php
@@ -4,9 +4,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Actualizar pago</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
               </div>
               <div class="modal-body">
                 <form action="#" method="POST" id="dataForm" data-saleID={{$sale->id}}>
