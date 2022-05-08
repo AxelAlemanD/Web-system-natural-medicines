ef92b3f9ac61af3f742dd72b3b9b2cbb4144753c
AxelAlemanD <axelad38@gmail.com>
2022-05-08 12:26:52 -0500

    add: Controller and view for dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,24 @@
+@extends('app')
+
+@section('content')
+
+<!-- CABECERA -->
+<div class="page-header d-flex d-block">
+    <div class="page-leftheader">
+        <h5>Hola Yolanda, bienvenida</h5>
+        <h4 class="page-title">Inicio</h4>
+    </div>
+</div>
+<!-- FIN CABECERA -->
+
+
+
+
+<!-- CONTENIDO -->
+<div class="row">
+    <div class="col-xl-12 col-md-12 col-lg-12">
+        
+    </div>
+</div>
+<!-- FIN CONTENIDO -->
+@endsection
